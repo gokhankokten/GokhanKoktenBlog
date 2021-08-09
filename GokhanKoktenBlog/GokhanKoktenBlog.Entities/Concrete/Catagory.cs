c5b61a927e4b1df9cf54fb9a4d8073006f90f39a
gokhankokten <88688756+gokhankokten@users.noreply.github.com>
2021-08-10 01:45:22 +0300

    Catagory sınıfı Oluşturuldu - 4

---
+++ GokhanKoktenBlog/GokhanKoktenBlog.Entities/Concrete/Catagory.cs
@@ -0,0 +1,16 @@
+﻿using GokhanKoktenBlog.Shared.Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GokhanKoktenBlog.Entities.Concrete
+{
+    public class Catagory : EntitiyBase,IEntity
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public ICollection<Article> Articles { get; set; }
+    }
+}
