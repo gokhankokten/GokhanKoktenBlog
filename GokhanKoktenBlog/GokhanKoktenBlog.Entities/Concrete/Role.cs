577e8fef834e28cfe1f5f95106a5f0bff97c611d
gokhankokten <88688756+gokhankokten@users.noreply.github.com>
2021-08-10 02:04:22 +0300

    Role ve User Sınıfları Oluşturuldu - 6

---
+++ GokhanKoktenBlog/GokhanKoktenBlog.Entities/Concrete/Role.cs
@@ -0,0 +1,17 @@
+﻿using GokhanKoktenBlog.Shared.Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GokhanKoktenBlog.Entities.Concrete
+{
+    public class Role:EntityBase,IEntity
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public ICollection<User> Users { get; set; }
+
+    }
+}
