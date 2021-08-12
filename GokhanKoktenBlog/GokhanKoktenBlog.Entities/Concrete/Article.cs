b6997323d4550814b352f2af0b234598eb3328ed
gokhankokten <88688756+gokhankokten@users.noreply.github.com>
2021-08-12 22:37:21 +0300

    Fluent Api Veritabanı için Mapping İşlemi yapıldı - B4

--- GokhanKoktenBlog/GokhanKoktenBlog.Entities/Concrete/Article.cs
+++ GokhanKoktenBlog/GokhanKoktenBlog.Entities/Concrete/Article.cs
@@ -13,13 +13,13 @@ namespace GokhanKoktenBlog.Entities.Concrete
         public string Content { get; set; }
         public string Thumbnail { get; set; }
         public DateTime Date { get; set; }
-        public int ViewsCount { get; set; }
+        public int ViewCount { get; set; }
         public int CommentCount { get; set; }
         public string SeoAuthor { get; set; }
         public string SeoDescription { get; set; }
         public string SeoTags { get; set; }
         public int CategoryId { get; set; }
-        public Category Catagory { get; set; }
+        public Category Category { get; set; }
         public int UserId { get; set; }
         public User User { get; set; }
         public ICollection<Comment> Comments { get; set; }
