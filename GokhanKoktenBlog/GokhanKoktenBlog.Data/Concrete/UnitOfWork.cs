3b5ead50c7c357b290706d71a3bf12ba769e01ac
gokhankokten <88688756+gokhankokten@users.noreply.github.com>
2021-08-12 23:15:52 +0300

    UnitOfWork Yapısını kurduk - B5

---
+++ GokhanKoktenBlog/GokhanKoktenBlog.Data/Concrete/UnitOfWork.cs
@@ -0,0 +1,46 @@
+﻿using GokhanKoktenBlog.Data.Abstract;
+using GokhanKoktenBlog.Data.Concrete.EnityFremework.Contexts;
+using GokhanKoktenBlog.Data.Concrete.EnityFremework.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GokhanKoktenBlog.Data.Concrete
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly GokhanKoktenBlogContext _context;
+        private EfArticleRepository _articleRepository;
+        private EfCategoryRepository _categoryRepository;
+        private EfCommentRepository _commentRepository;
+        private EfRoleRepository _roleRepository;
+        private EfUserRepository _userRepository;
+
+        public UnitOfWork(GokhanKoktenBlogContext context)
+        {
+            _context = context;
+        }
+
+        public IArticleRepository Articles => _articleRepository ?? new EfArticleRepository(_context);
+
+        public ICategoryRepository Categories => _categoryRepository ?? new EfCategoryRepository(_context);
+        public ICommentRepository Comments => _commentRepository ?? new EfCommentRepository(_context);
+
+        public IRoleRepository Roles => _roleRepository ?? new EfRoleRepository(_context);
+
+        public IUserRepository Users => _userRepository ?? new EfUserRepository(_context);
+
+ 
+        public async Task<int> SaveAsync()
+        {
+            return await _context.SaveChangesAsync();
+        }
+
+        public async ValueTask DisposeAsync()
+        {
+            await _context.DisposeAsync();
+        }
+    }
+}
