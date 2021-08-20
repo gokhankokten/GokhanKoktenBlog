3eadb23c2981fda4b6c4e3b77ec330eafded3b06
gokhankokten <88688756+gokhankokten@users.noreply.github.com>
2021-08-20 23:11:16 +0300

    Category Controller index View eklendi - 44
    Co-Authored-By: Fatih Altuntaş <36915172+fatihaltuntas@users.noreply.github.com>

---
+++ GokhanKoktenBlog/GokhanKoktenBlog.Mvc/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,32 @@
+﻿using GokhanKoktenBlog.Services.Abstract;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GokhanKoktenBlog.Shared.Utilities.Results.ComplexTypes;
+
+namespace GokhanKoktenBlog.Mvc.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CategoryController : Controller
+    {
+
+        private readonly ICategoryService _catetegoryService;
+
+        public CategoryController(ICategoryService catetegoryService)
+        {
+            _catetegoryService = catetegoryService;
+        }
+
+        public async Task <IActionResult> Index()
+        {
+            var result = await _catetegoryService.GetAll();
+            if (result.ResultStatus==ResultStatus.Success)
+            {
+                return View(result.Data);
+            }
+            return View();
+        }
+    }
+}
